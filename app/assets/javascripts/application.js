7d7a800ef130b58cd549d4cead93be41a47df60e
chris mudongo <chrismudongo@gmail.com>
2018-08-07 10:39:35 +0200

    resolving gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require cocoon
 //= require rails-ujs
 //= require activestorage
