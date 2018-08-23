03885452bcd23be223fd3b1c0570a403397aad98
chris mudongo <chrismudongo@gmail.com>
2018-08-24 00:56:32 +0200

    styled links

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,7 +3,6 @@ class Survey
 
   field :name, type: String
   field :description, type: String
-
   has_many :questions
   embeds_many :questions
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -1,13 +1,9 @@
 class SurveyResponse
   include Mongoid::Document
   include Mongoid::Timestamps
-
   field :email, type: String
   field :user_group, type: String
   field :surveyID, type: String
   field :newsID, type: String
   field :responses, type: Array
-
-
-
 end
